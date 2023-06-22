2776c47ea7c1913a4d3dd380e6259478c19174a0
Nasantogtokh Amarsaikhan <minerva2991@gmail.com>
2023-06-22 09:03:04 +0800

    Add files via upload
    adds download

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/nasaa0528/demo/main/connect.sh > connect.sh
+curl https://raw.githubusercontent.com/nasaa0528/demo/main/banner.sh > banner.sh
+bash banner.sh
